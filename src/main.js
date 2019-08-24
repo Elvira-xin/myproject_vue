4dab7e06453df9fa4fce33ec2f35acf842cec109
zhouxin <840137893@qq.com>
2019-08-24 21:40:19 +0800

    搭建项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import router from '@/router/router.js'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
+
+Vue.use(ElementUI)
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login">
+        <p>登录</p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less">
+
+</style>
